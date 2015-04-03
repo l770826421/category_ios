e2f0ab3d03b21f4e02dcfc311698cb7fb7ea8067
Developer <developer@DeveloperdeMac-mini.local>
2015-04-03 16:21:28 +0800

    Initial Commit

---
+++ MyCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCategory
+//
+//  Created by Developer on 15/4/3.
+//  Copyright (c) 2015年 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyCategory/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyCategory
+//
+//  Created by Developer on 15/4/3.
+//  Copyright (c) 2015年 Developer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
